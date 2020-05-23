832674ecab8f76fc6e7af04c893312ff98f2a208
Daryl <daryltan900@gmail.com>
2020-05-23 20:29:28 +0800

    allow for context to be set

--- Bot.py
+++ Bot.py
@@ -8,38 +8,48 @@ import numpy as np
 import pandas as pd
 import random
 
+
 class Bot():
     def __init__(self):
         with open("data.pickle", "rb") as f:
             self.words, self.intents, self.classes = pickle.load(f)
         self.model = keras.models.load_model('model.tflearn')
         self.stemmer = LancasterStemmer()
+        self.context = []
 
-    def bow(self, sentence, show_details=True):
+    def bow(self, inpt):
         # tokenize the pattern
-        sentence_words = nltk.word_tokenize(sentence)
+        inpt_words = nltk.word_tokenize(inpt)
         # stem each word - create short form for word
-        sentence_words = [self.stemmer.stem(word.lower()) 
-        for word in sentence_words]
+        inpt_words = [self.stemmer.stem(word.lower())
+                      for word in inpt_words]
         # bag of words - matrix of N words, vocabulary matrix
-
         bag = [0] * len(self.words)
-        for s in sentence_words:
+        for s in inpt_words:
             for i, w in enumerate(self.words):
                 if w == s:
-                   bag[i] = 1
+                    bag[i] = 1
         return pd.DataFrame([np.array(bag)], dtype=float, index=['input'])
 
+    def classify(self, inpt):
+        results = self.model.predict([self.bow(inpt)])[0]
+        results = [[i, r] for i, r in enumerate(results) if r > 0.5]
+        results.sort(key=lambda x: x[1], reverse=True)
+        lst = []
+        for r in results:
+            lst.append((self.classes[r[0]], r[1]))
+        return lst
 
-    def respond(self, inpt):
-        results = self.model.predict(self.bow(inpt))
-        results_index = np.argmax(results)
-        tag = self.classes[results_index]
+    def response(self, inpt):
+        results = self.classify(inpt)
+        while results:
+            for i in self.intents['intents']:
+                if i['tag'] == results[0][0]:
+                    if 'context_set' in i:
+                        self.context.append(i['context_set'])
 
-        for tg in self.intents["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
-                #print(tag, '\n')
-                break
+                    if not 'context_cond' in i or \
+                            ('context_cond' in i and i['context_cond'] in self.context):
+                        return (random.choice(i['responses']))
 
-        return (random.choice(responses))
+            results.pop(0)
--- TF_Model.py
+++ TF_Model.py
@@ -1,3 +1,11 @@
+import json
+import random
+import pickle
+import pandas as pd
+from keras.optimizers import SGD
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+import numpy as np
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 
@@ -5,85 +13,79 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 stemmer = LancasterStemmer()
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import SGD
-import pandas as pd
-import pickle
-import random
-import json
 
 with open('intents.json') as json_data:
     intents = json.load(json_data)
 
-    words = []
-    classes = []
-    docs = []
+words = []
+classes = []
+docs = []
 
-    # loop through each sentence in our intents patterns
-    for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            # tokenize each word in the sentence
-            w = nltk.word_tokenize(pattern)
-            # add to our words list
-            words.extend(w)
-            # add to docs in our corpus
-            docs.append((w, intent['tag']))
-            # add to our classes list
-            if intent['tag'] not in classes:
-                classes.append(intent['tag'])
+# loop through each sentence in our intents patterns
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # tokenize each word in the sentence
+        w = nltk.word_tokenize(pattern)
+        # add to our words list
+        words.extend(w)
+        # add to docs in our corpus
+        docs.append((w, intent['tag']))
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
-    # stem and lower each word and remove duplicates
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
+# stem and lower each word and remove duplicates
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
 
-    # sort classes
-    classes = sorted(list(set(classes)))
+# sort classes
+classes = sorted(list(set(classes)))
 
-    # create our training data
-    training = []
-    # create an empty array for our output
-    output_empty = [0] * len(classes)
+# create our training data
+training = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
 
-    # training set, bag of words for each sentence
-    for doc in docs:
-        # initialize our bag of words
-        bag = []
-        # list of tokenized words for the pattern
-        pattern_words = doc[0]
-        # stem each word - create base word, in attempt to represent related words
-        pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-        # create our bag of words array with 1, if word match found in current pattern
-        for w in words:
-            bag.append(1) if w in pattern_words else bag.append(0)
+# training set, bag of words for each sentence
+for doc in docs:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # stem each word - create base word, in attempt to represent related words
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
 
-        # output is a '0' for each tag and '1' for current tag (for each pattern)
-        output_row = list(output_empty)
-        output_row[classes.index(doc[1])] = 1
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
 
-        training.append([bag, output_row])
+    training.append([bag, output_row])
 
-    # shuffle our features and turn into np.array
-    random.shuffle(training)
-    training = np.array(training)
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
 
-    # create train and test lists. X - patterns, Y - intents
-    train_x = list(training[:, 0])
-    train_y = list(training[:, 1])
+# create train and test lists. X - patterns, Y - intents
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 
-    model = Sequential()
-    model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(train_y[0]), activation='softmax'))
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
 
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd, metrics=['accuracy'])
 
 
-model.fit(np.array(train_x), np.array(train_y), epochs=1000, batch_size=5, verbose=1)
+model.fit(np.array(train_x), np.array(train_y),
+          epochs=1000, batch_size=5, verbose=1)
 model.save("model.tflearn")
 
 
