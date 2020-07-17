c9c9a675b5056aa2d4b34fd7143b5261cf0eddc3
Bryan <tteo43@outlook.com>
2020-07-17 15:09:34 +0800

    added new test case for automated testing

--- testing.py
+++ testing.py
@@ -27,31 +27,33 @@ def check(inpt, tag):
     respondedTag = bot.respond(inpt, True)
 
     if tag == "context" and respondedTag == "May I enquire which faculty are you referring to?":
-        print(tag, "Correct\n")
+        print("{}: Correct".format(tag))
         return 1 
     elif tag == respondedTag:
-        print(tag, "Correct\n")
+        print("{}: Correct".format(tag))
         return 1
     else:
-        print(tag, respondedTag, "Incorrect\n")
+        print("{}: Incorrect \n Returned: {}".format(tag, respondedTag))
         return 0
 
-respond("What are the double major offered by nus")
-respond("Computing")
-respond("What are the specialization available in computer science")
-respond("What are the specialization available in business analytics")
-respond("What are the degree offered by nus computing")
+respond("What are the double major offered by NUS?")
+respond("NUS Computing")
+respond("What are the specialization available in computer science?")
+respond("What are the specialization available in business analytics?")
+respond("What are the degree offered by nus computing?")
 respond("How many modular credits is required for a major?")
-respond("What is the outcome of my application")
+respond("What is the outcome of my application?")
+respond("Who do I contact for more information on admission?")
 
 tags = ["context", "DoubleMajorOffered", "ComSci", "BZA",
-"ComCourses", "major", "Application_Outcome"]
+"ComCourses", "major", "Application_Outcome", "Admission Information"]
 
 queries = driver.find_elements_by_id("usermsg")
 queries = zip(tags, queries)
 
-count = 0
+score = 0
 for tag, query in queries:
-    print("\n", query.text, "\n")
-    count += check(query.text, tag)
-print("Total Number of Correct Respones: " + str(count) + " / 7")
+    print("\n", query.text)
+    score += check(query.text, tag)
+    
+print("\nTotal Number of Correct Respones: {}/{}".format(score, len(tags)))
